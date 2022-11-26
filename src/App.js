13218bd86d5148c1c7a3955244f605f94ff00238
Vaibhav <vaibhavchaubey8802@gmail.com>
2022-11-26 15:04:25 +0530

    Hooks Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React from "react";
 import Resturant from "./component/Basics/Resturant";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
 
 const App = () => {
   return (
     <Resturant />
-  )
-}
 
-export default App
+    /* Hooks in React JS */
+    // <UseState />
+    // <UseEffect />
+    // <UseReducer />
+  );
+};
+
+export default App;
